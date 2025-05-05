a3675c8d6d96b6141f59016234097891ad0a525b
Matthias Arnold <42539387+DrMatthiasArnold@users.noreply.github.com>
2025-05-05 19:53:02 +0200

    pH-Meter: added license file and headers

--- servers/lads-ph-meter/src/server.ts
+++ servers/lads-ph-meter/src/server.ts
@@ -1,13 +1,23 @@
 // SPDX-FileCopyrightText: 2025 Dr. Matthias Arnold, AixEngineers, Aachen, Germany.
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL 3
 
-/**
- *
- * Copyright (c) 2025 Dr. Matthias Arnold, AixEngineers, Aachen, Germany.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+/*
+LADS pH-Meter
+Copyright (C) 2025  Dr. Matthias Arnold, AixEngineers, Aachen, Germany.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 import { ApplicationType, assert, coerceNodeId, DataType, OPCUAServer, UAObject } from "node-opcua"
 import { join } from "path"
